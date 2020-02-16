03fb54f8b62146ad1db54d7eaed780a0836d3348
valakorn <valakorn.t@gmail.com>
2020-02-16 10:21:16 +0700

    no message

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,6 +14,7 @@ func main() {
 	http.HandleFunc("/todos", todosHandler)
 	http.HandleFunc("/todos/new", newHandler)
 	http.HandleFunc("/todos/create", createHandler)
+	http.Handle("/static/", http.FileServer(http.Dir("ccs")))
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -19,7 +23,7 @@ func todosHandler(w http.ResponseWriter, r *http.Request) {
 	// 	return parseFiles(nil, filenames...)
 	// }
 	// t, err := template.ParseFiles("index.html")
-	t := template.Must(template.ParseFiles("index.html"))
+	t := template.Must(template.ParseFiles("index.html", "todo.html"))
 
 	// if err != nil {
 
@@ -27,11 +31,11 @@ func todosHandler(w http.ResponseWriter, r *http.Request) {
 	// }
 
 	//t.Execute(w, []string{"learn golang", "practise execercise", "make coffee"})
-	t.Execute(w, readLines)
+	t.Execute(w, readLines("todos.txt"))
 	//w.Write([]byte("hello"))
 }
 func newHandler(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles("new.html"))
+	t := template.Must(template.ParseFiles("index.html", "new.html"))
 	t.Execute(w, nil)
 
 }
@@ -39,7 +43,15 @@ func createHandler(w http.ResponseWriter, r *http.Request) {
 	//t := template.Must(template.ParseFiles("new.html"))
 	//t.Execute(w, nil)
 	//r.ParseForm()
-	log.Println(r.PostForm["item"])
+	//log.Println(r.PostForm["item"])
+	log.Println(r.FormValue("item"))
+
+	f, err := os.OpenFile("todos.txt", os.O_APPEND, os.ModeAppend)
+	check_err(err)
+	_, err = fmt.Fprintln(f, r.FormValue("item"))
+	check_err(err)
+
+	http.Redirect(w, r, "/todos", http.StatusFound)
 
 }
 
@@ -48,6 +60,20 @@ func check_err(err error) {
 		panic(err)
 	}
 }
-func readLines(fn string) []string {
-	return []string{"a", "b", "c"}
+func readLines(name string) []string {
+	f, err := os.Open(name)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check_err(err)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check_err(scanner.Err())
+
+	return lines
 }
